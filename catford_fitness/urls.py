4705f8d319ca17cd0763c69bfc496e4e7dfd4919
oakerele-web <omoakerele@gmail.com>
2021-07-14 06:21:37 +0000

    added gym html to the main page

--- catford_fitness/urls.py
+++ catford_fitness/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('memberships/', include('memberships.urls')),   
+    path('gym/', include('gym.urls')),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gym(request):
+    """ A view to return the index page """
+
+    return render(request, 'gym/gym.html')
