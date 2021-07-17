b7516d85794469f83561702bb0c55cfe43aad339
oakerele-web <omoakerele@gmail.com>
2021-07-17 19:47:41 +0000

    removed secret key and set debug

--- catford_fitness/settings.py
+++ catford_fitness/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52vrl&%!if7runaey3&c*%ut!(o1*h(6%z9d*iafm@c9$tt*39'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['oakerele-catford-fitness.herokuapp.com', 'localhost']
 
